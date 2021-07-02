3f33223125d58f4a20ea5030b0daf549a5e69c44
Luis Antonio García Perrusquía <lgarciaperrusquia@gmail.com>
2021-07-02 18:15:32 -0500

    EditUser

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,6 +1,8 @@
 const bcrypt = require("bcrypt-nodejs");
 const jwt = require('../services/jwt');
 const User = require("../models/user");
+const fs = require('fs');
+const path = require('path');
 
 function singUp(req, res) {
     const user = new User();
@@ -104,9 +106,90 @@ function getUsersActive(req, res) {
     });
 };
 
+function uploadAvatar(req, res) {
+    const params = req.params;
+    User.findById({ _id: params.idUsuario }, (err, userData) => {
+        if (err) {
+            res.status(500).send({ success: 0, message: 'Error del servidor' });
+        } else {
+            if (!userData) {
+                res.status(404).send({ success: 0, message: 'No se encontro ningun usuario' });
+            } else {
+                let user = userData;
+
+                if (req.files) {
+                    let filePath = req.files.avatar.path;
+                    let fileName = filePath.split('\\')[2];
+                    let fileExt = fileName.split(".")[1];
+
+                    if (fileExt !== 'png' && fileExt !== 'jpg') {
+                        res.status(400).send({ success: 0, message: 'La extension de la imagen no es valida, solo se permiten "png" y "jpg".' });
+                    } else {
+                        user.avatar = fileName;
+                        User.findByIdAndUpdate({ _id: params.idUsuario }, user, (error, userRes) => {
+                            if (error) {
+                                res.status(500).send({ success: 0, message: 'Error del servidor' });
+                            } else {
+                                if (!userRes) {
+                                    res.status(404).send({ success: 0, message: 'Usuario no encontrado' });
+                                } else {
+                                    res.status(200).send({ success: 1, message: 'Se guardo la imagen con éxito', data: fileName });
+                                };
+                            };
+                        });
+                    };
+                };
+            };
+        };
+    });
+};
+
+function getUrlAvatar(req, res) {
+    const avatarName = req.params.avatarName;
+    const filePath = `./uploads/avatar/${avatarName}`;
+
+    fs.exists(filePath, exists => {
+        if (!exists) {
+            res.status(404).send({ success: 0, message: 'No se encontro el avatar' });
+        } else {
+            res.sendFile(path.resolve(filePath));
+        };
+    });
+};
+
+async function updateUser(req, res) {
+    let userData = req.body;
+    userData.email = req.body.email.toLowerCase();
+    const { idUsuario } = req.params;
+    if (userData.password) {
+        await bcrypt.hash(userData.password, null, null, (err, hash) => {
+            if (err) {
+                res.status(500).send({ success: 0, message: 'Error al encriptar la contraseña.' });
+            } else {
+                userData.password = hash;
+            };
+        });
+    };
+
+    User.findByIdAndUpdate({ _id: idUsuario }, userData, (error, userRes) => {
+        if (error) {
+            res.status(500).send({ success: 0, message: 'Error del servidor.' });
+        } else {
+            if (!userRes) {
+                res.status(404).send({ success: 0, message: 'No se encotro el usuario.' });
+            } else {
+                res.status(200).send({ success: 1, message: 'El usuario se actualizo con éxito.' });
+            };
+        };
+    });
+};
+
 module.exports = {
     singUp,
     signIn,
     getUsers,
-    getUsersActive
+    getUsersActive,
+    uploadAvatar,
+    getUrlAvatar,
+    updateUser
 };
--- server/routers/user.js
+++ server/routers/user.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const UserController = require('../controllers/user');
 const md_auth = require('../middleware/autheticated');
+const multipart = require('connect-multiparty');
 
 const api = express.Router();
+const md_upload_avatar = multipart({ uploadDir: './uploads/avatar' })
 
 api.post("/sign-up", UserController.singUp);
 api.post("/sign-in", UserController.signIn);
 api.get("/users", [md_auth.ensureAuth], UserController.getUsers);
 api.get("/users-active", [md_auth.ensureAuth], UserController.getUsersActive);
+api.put("/upload-avatar/:idUsuario", [md_auth.ensureAuth, md_upload_avatar], UserController.uploadAvatar);
+api.get("/get-url-avatar/:avatarName", UserController.getUrlAvatar);
+api.put("/update-user/:idUsuario", [md_auth.ensureAuth], UserController.updateUser);
 
 module.exports = api;
